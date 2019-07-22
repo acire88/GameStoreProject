28822e5fa2507afe46a9052f2a64bec1989b32b4
Aonthemove <ambergipson86@gmail.com>
2019-07-22 13:51:33 -0500

    first commit

---
+++ src/main/java/com/company/GameStoreProject/GameRepo.java
@@ -0,0 +1,10 @@
+package com.company.GameStoreProject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface GameRepo extends JpaRepository<Games, Integer> {
+    List<Games> findByESRBRating(String ESRBRating);
+    List<Games> findByStudio(String studio);
+    List<Games> findByTitle(String title);
+}
+
